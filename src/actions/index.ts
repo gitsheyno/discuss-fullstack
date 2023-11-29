573b4a565afa35f696380e84ded1c28308dc265b
shayan heidary <hdrydeveloper@gmail.com>
2023-11-29 20:27:59 +0100

    Header ServerActions seperated

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,11 +1,5 @@
-"use server";
-
-import * as auth from "@/auth";
-
-export async function signIn() {
-  return auth.signIn("github");
-}
-
-export async function signOut() {
-  return auth.signOut();
-}
+export { signIn } from "./signIn";
+export { signOut } from "./signOut";
+export { createComment } from "./createComment";
+export { createTopic } from "./createTopic";
+export { createPost } from "./createPost";
---
+++ src/components/Header.tsx
@@ -0,0 +1,36 @@
+import Link from "next/link";
+// import * as path from "@/path";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Input,
+  Button,
+  Avatar,
+} from "@nextui-org/react";
+import { auth } from "@/auth";
+
+export default async function Header() {
+  const session = await auth();
+
+  return (
+    <Navbar className="shadow mb-6">
+      <NavbarBrand>
+        <Link href="/" className="font-bold">
+          Discuss
+        </Link>
+      </NavbarBrand>
+      <NavbarContent justify="center">
+        <NavbarItem>
+          <Input />
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarContent justify="end">
+        <NavbarItem>
+          {session?.user ? <div>sign in</div> : <div>sign out</div>}
+        </NavbarItem>
+      </NavbarContent>
+    </Navbar>
+  );
+}
