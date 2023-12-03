3ff0869880be5a99f74fb1f167a18d98cd8b01b7
shayan heidary <hdrydeveloper@gmail.com>
2023-12-03 21:36:35 +0100

    fixed cached for fetching database and create comment completed

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,5 +1,5 @@
 export { signIn } from "./signIn";
 export { signOut } from "./signOut";
-export { createComment } from "./createComment";
+export { createComment } from "./create-comment";
 export { createTopic } from "./createTopic";
 export { createPost } from "./createPost";
--- src/db/queries/comments.ts
+++ src/db/queries/comments.ts
@@ -1,22 +1,23 @@
 import { Comment } from "@prisma/client";
 import prisma from "@/db";
+import { cache } from "react";
 
 export type CommentWithAuthor = Comment & {
   user: { name: string | null; image: string | null };
 };
 
-export function fetchCommentsByPostId(
-  postid: string
-): Promise<CommentWithAuthor[]> {
-  return prisma.comment.findMany({
-    where: { postId: postid },
-    include: {
-      user: {
-        select: {
-          name: true,
-          image: true,
+export const fetchCommentsByPostId = cache(
+  (postid: string): Promise<CommentWithAuthor[]> => {
+    return prisma.comment.findMany({
+      where: { postId: postid },
+      include: {
+        user: {
+          select: {
+            name: true,
+            image: true,
+          },
         },
       },
-    },
-  });
-}
+    });
+  }
+);
